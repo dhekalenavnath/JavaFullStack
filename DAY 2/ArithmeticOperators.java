4173b950de186c41e8920751f94f63fbbe66f680
Navnath Dhekale <navnathdhekale9373@gmail.com>
2025-07-08 23:32:14 +0530

    Learned Operaters  and its use

---
+++ DAY 2/ArithmeticOperators.java
@@ -0,0 +1,19 @@
+
+
+public class ArithmeticOperators {
+    public static void main(String args[]){
+        int a=18,b=10;
+        
+        System.out.println("a + b = " + (a + b)); // Addition
+        System.out.println("a - b = " + (a - b)); // Subtraction
+        System.out.println("a * b = " + (a * b)); // Multiplication
+        System.out.println("a / b = " + (a / b)); // Division
+        System.out.println("a % b = " + (a % b)); // Modulus
+
+        System.out.println("a++ = " + (a++)); // Post-increment
+        System.out.println("++a = " + (++a)); // Pre-increment
+        System.out.println("a-- = " + (a--)); // Post-decrement
+        System.out.println("--a = " + (--a)); // Pre-decrement
+        
+    }
+}
---
+++ DAY 2/RelationalOperaters.java
@@ -0,0 +1,12 @@
+public class RelationalOperaters {
+    public static void main(String args[]){
+        int a=18,b=10;
+        // Note: Relational operators compare two values and return a boolean result.
+        System.out.println("a == b : " + (a == b)); // Equal to
+        System.out.println("a != b : " + (a != b)); // Not equal to
+        System.out.println("a > b : " + (a > b));   // Greater than
+        System.out.println("a < b : " + (a < b));   // Less than
+        System.out.println("a >= b : " + (a >= b)); // Greater than or equal to
+        System.out.println("a <= b : " + (a <= b)); // Less than or equal to
+    }
+}
